55195bfeeaa158e6a0689db45acf62c1fdd60722
xiangpin <xshuangbin@163.com>
2021-10-08 15:40:49 +0800

    make a structure of package

---
+++ R/AllData.R
@@ -0,0 +1,12 @@
+#' @title tree (phylo class) to display with multiple sequence alignment
+#'
+#' @description 
+#' This tree is used to display with multiple sequence alignment
+#'
+#' @name data-tree.nwk
+#' @aliases tree.nwk
+#' @docType data
+#' @keywords data
+#' @examples
+#' data(tree.nwk)
+NA
