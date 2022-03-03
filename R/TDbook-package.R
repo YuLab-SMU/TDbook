10ca3f5fe14adb90e78ab53315c7134d01b5d2f8
Guangchuang Yu <guangchuangyu@gmail.com>
2022-03-03 15:43:40 +0800

    packag doc

---
+++ R/TDbook-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' Companion Package for the Book 
+#' "Data Integration, Manipulation and Visualization of Phylogenetic Trees" 
+#' by Guangchuang Yu (2022, ISBN:9781032233574).
+#'
+#' 
+#'
+#' @name TDbook-package
+#' @aliases TDbook-package TDbook
+#' @docType package
+NULL
